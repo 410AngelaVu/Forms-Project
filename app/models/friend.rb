c9674eb5699e0e6deabfde895f11755c770c74eb
410AngelaVu <andjela1993@gmail.com>
2020-11-30 23:53:48 +0100

    Fixing changes by TSE

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,2 +1,6 @@
 class Friend < ApplicationRecord
+
+validates :first_name, :last_name, :email, :password, presence: true
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_170935) do
+ActiveRecord::Schema.define(version: 2020_11_30_222143) do
 
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_11_30_170935) do
     t.string "twitter"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password"
   end
 
 end
