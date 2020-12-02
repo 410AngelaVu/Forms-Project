2b04e569c201a326527e35f0b21c2b0c9ba27c75
Cosmel Villalobos <cosmelvg@gmail.com>
2020-12-01 20:16:25 -0500

    Added the Edit page to the user and updated the routes and controller as well

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :users, only: [ :new, :create ]
+  # resources :users, only: [ :new, :create , :edit, :update ]
+  resources :users
   resources :friends
   get 'home/index'
    root 'home#index'
