2268faa103999ff3e049bb7d4834dab2677d9a98
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-06 18:05:29 -0400

    changed configuration to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,25 @@
 
 ActiveRecord::Schema.define(version: 2020_12_01_152544) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "author_name"
+    t.text "body"
+    t.bigint "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -23,6 +42,21 @@ ActiveRecord::Schema.define(version: 2020_12_01_152544) do
     t.string "password"
   end
 
+  create_table "taggings", force: :cascade do |t|
+    t.bigint "tag_id", null: false
+    t.bigint "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_taggings_on_article_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
@@ -31,4 +65,7 @@ ActiveRecord::Schema.define(version: 2020_12_01_152544) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "articles"
+  add_foreign_key "taggings", "articles"
+  add_foreign_key "taggings", "tags"
 end
